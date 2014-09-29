ddfcc193be63107b2b954af04654ef3f627e77ae
banco2 <edwilldave@yahoo.com>
2014-09-29 20:06:21 +0800

    Testing Xdebug

---
+++ debug.php
@@ -0,0 +1,19 @@
+<?php
+
+function isOfDrivingAge($age)
+{
+
+    return $age >= 17;
+}
+
+
+function  notifyUserOfDriverStatus($name, $age)
+{
+
+    $message = isOfDrivingAge($age) ? 'You may drive.' : 'You cant drive';
+
+    return "{$name}: {$message}";
+
+}
+
+$status = notifyUserOfDriverStatus('John Doe', 17);
